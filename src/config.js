a55b28c472934675c38a587862c18edb5a697358
Jonathan Marien <jon@chron0.tech>
2025-05-12 23:53:19 -0400

    chore: simplified config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 // Configure per-device API keys here
 // Example: export const API_KEYS = { device1: 'key1', device2: 'key2' };
 export const API_KEYS = {
-  mypc: process.env.API_KEY_MYPC,
+  //mypc: process.env.API_KEY_MYPC,
   thebeast: process.env.API_KEY_THEBEAST
   // Add more devices as needed
 };
